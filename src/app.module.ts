f96b5c3ab4322eb43480687f788d3eecf1bd37ac
David Misael Villegas Sandoval <david.misa97@gmail.com>
2024-03-08 21:04:37 -0300

    Update module imports in src/app.module.ts
    Reordered and updated the import paths.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,23 +5,23 @@ import { ConfigModule, ConfigService }            from '@nestjs/config';
 import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ThrottlerModule }                        from '@nestjs/throttler';
 
-import { CommonModule }     from '@common/common.module';
-import { AuthModule }       from '@modules/auth/auth.module';
-import { AuthGuard }        from '@modules/auth/guards/auth.guard';
-import { JwtModule }        from '@modules/jwt/jwt.module';
-import { MailerModule }     from '@modules/mailer/mailer.module';
-import { UsersModule }      from '@modules/users/users.module';
-import { Oauth2Module }     from '@modules/oauth2/oauth2.module';
-import { CacheConfig }      from '@config/cache.config';
-import { validationSchema } from '@config/config.schema';
-import { MikroOrmConfig }   from '@config/mikroorm.config';
-import { ThrottlerConfig }  from '@config/throttler.config';
-
-import { AppService }              from './app.service';
-import { config }                  from './config';
-import { HttpResponseInterceptor } from '@common/interceptors/http-response.interceptor';
-import { PermissionsModule }       from './modules/permissions/permissions.module';
+import { CommonModule }            from '@common/common.module';
 import { HttpExceptionFilter }     from '@common/filters/http-exception.filter';
+import { HttpResponseInterceptor } from '@common/interceptors/http-response.interceptor';
+import { AuthModule }              from '@modules/auth/auth.module';
+import { AuthGuard }               from '@modules/auth/guards/auth.guard';
+import { JwtModule }               from '@modules/jwt/jwt.module';
+import { MailerModule }            from '@modules/mailer/mailer.module';
+import { PermissionsModule }       from '@modules/permissions/permissions.module';
+import { UsersModule }             from '@modules/users/users.module';
+import { Oauth2Module }            from '@modules/oauth2/oauth2.module';
+import { CacheConfig }             from '@config/cache.config';
+import { validationSchema }        from '@config/config.schema';
+import { MikroOrmConfig }          from '@config/mikroorm.config';
+import { ThrottlerConfig }         from '@config/throttler.config';
+
+import { AppService } from './app.service';
+import { config }     from './config';
 
 @Module({
   imports: [
