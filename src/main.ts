677ec23b12a81c2414d72d85c00f613cc1062a92
David Misael Villegas Sandoval <david.misa97@gmail.com>
2024-05-05 22:24:01 -0400

    Add Fastify compression dependency and update dependencies.
    The commit includes the addition of Fastify compression plugin to the project's dependencies. As a result, package-lock.json has been updated to reflect this change. Some other dependencies have also been updated.

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import fastifyCookie         from '@fastify/cookie';
 import fastifyHelmet         from '@fastify/helmet';
 
 import { AppModule } from './app.module';
+import fastifyCors   from '@fastify/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -19,23 +20,22 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   app.setGlobalPrefix('api');
-  await app.register(fastifyCookie as any, {
-    secret: configService.get<string>('COOKIE_SECRET')
-  });
+  await app.register(import('@fastify/compress'), {global: false});
+  await app.register(fastifyCookie as any, {secret: configService.get<string>('COOKIE_SECRET')});
   await app.register(fastifyHelmet as any);
   await app.register(fastifyCsrfProtection as any, {cookieOpts: {signed: true}});
-  // await app.register(fastifyCors as any, {
-  //   credentials: true,
-  //   origin: '*'
-  // });
-
-  app.enableCors({
+  await app.register(fastifyCors as any, {
     credentials: true,
-    preflightContinue: true,
-    origin: '*',
-    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: '*'
   });
+
+  // app.enableCors({
+  //   credentials: true,
+  //   preflightContinue: true,
+  //   origin: '*',
+  //   allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  // });
 
   app.useGlobalPipes(
     new ValidationPipe({
