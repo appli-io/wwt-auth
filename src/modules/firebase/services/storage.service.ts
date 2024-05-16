b2491a94ed1a8b7eecfefe60154e2b249fffa5d3
David Misael Villegas Sandoval <david.misa97@gmail.com>
2024-05-15 21:44:41 -0400

    Update file retrieval method in StorageService
    The changes made to the storage.service.ts file involve refactoring the getFiles method.
    
    Changes:
    + Imported GetSignedUrlConfig from @google-cloud/storage instead of importing File
    + Updated getFiles() method to be asynchronous
    + Replaced getDownloadURL() with getSignedUrl() method for generating file URLs
    + Added a configuration object for getSignedUrl() method
    + Returned more file details (name, url, contentType, size, timeCreated, updated) from getFiles() method.

--- src/modules/firebase/services/storage.service.ts
+++ src/modules/firebase/services/storage.service.ts
@@ -1,22 +1,36 @@
 import { Injectable } from '@nestjs/common';
 
-import { Bucket, File }               from '@google-cloud/storage';
-import { getDownloadURL, getStorage } from 'firebase-admin/storage';
+import { Bucket }             from '@google-cloud/storage';
+import { getStorage }         from 'firebase-admin/storage';
+import { GetSignedUrlConfig } from '@google-cloud/storage/build/cjs/src/file';
 
 @Injectable()
 export class StorageService {
   private _storage: Bucket;
 
-  getFiles() {
+  async getFiles() {
     if (!this._storage) {
       this._storage = getStorage().bucket();
     }
 
-    return this._storage.getFiles().then((files) => {
-      return files.map((file) => {
-        console.log(file);
-        return getDownloadURL(file['metadata'] as File);
-      });
-    });
+    const options: GetSignedUrlConfig = {
+      version: 'v4',
+      action: 'read',
+      expires: Date.now() + 3600 * 1000,
+    };
+
+    const files = (await this._storage.getFiles())[0];
+
+    console.log(files);
+    return await Promise.all(
+      files.map(async file => ({
+        name: file.name,
+        url: (await file.getSignedUrl(options))[0], // Accede al primer elemento del array
+        contentType: file.metadata.contentType,
+        size: file.metadata.size,
+        timeCreated: file.metadata.timeCreated,
+        updated: file.metadata.updated,
+      }))
+    );
   }
 }
