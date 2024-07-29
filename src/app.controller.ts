8219cb9abf88e6d898d8f29cd695fa1083117221
David Misael Villegas Sandoval <david.misa97@gmail.com>
2024-07-28 21:34:10 -0400

    Add user seeding functionality and update configurations
    Implement user creation DTO and enhance the AppService to include
    user seeding. Update configuration files to support new user password
    feature.
    
    ### src/app.service.ts
    + Import UsersService and OAuthProvidersEnum.
    + Define CreateUserDto interface for user attributes.
    + Add seedUsers method to create and seed an admin user.
    
    ### src/config/index.ts
    + Add prefilledUserPassword to configuration settings.
    
    ### src/config/interfaces/config.interface.ts
    + Include prefilledUserPassword in IConfig interface.
    
    ### src/modules/users/users.service.ts
    + Modify create method to accept confirmed parameter.
    + Update isConfirmed logic to consider confirmed status.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AppService }      from './app.service';
 
 @Controller()
 export class AppController {
 
+  constructor(private readonly _appService: AppService) {}
+
+  @Get()
+  seedUsers() {
+    return this._appService.seedUsers();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,21 +1,37 @@
-import { MikroORM }                                                          from '@mikro-orm/core';
 import { Injectable, Logger, LoggerService, OnModuleDestroy, OnModuleInit, } from '@nestjs/common';
+import { ConfigService }                                                     from '@nestjs/config';
+
+import { MikroORM } from '@mikro-orm/core';
+
+import { IConfig }            from '@config/interfaces/config.interface';
+import { OAuthProvidersEnum } from '@modules/users/enums/oauth-providers.enum';
+import { UsersService }       from '@modules/users/users.service';
+
+export interface CreateUserDto {
+  provider: OAuthProvidersEnum;
+  email: string;
+  name: string;
+  password: string;
+  confirmed: boolean;
+}
 
 @Injectable()
 export class AppService implements OnModuleInit, OnModuleDestroy {
   private readonly loggerService: LoggerService;
 
   constructor(
-    private readonly orm: MikroORM,
+    private readonly _orm: MikroORM,
+    private readonly _userService: UsersService,
+    private readonly _configService: ConfigService<IConfig>,
   ) {
     this.loggerService = new Logger(AppService.name);
   }
 
   public async onModuleInit() {
-    const updateDump = await this.orm.getSchemaGenerator().getUpdateSchemaSQL();
+    const updateDump = await this._orm.getSchemaGenerator().getUpdateSchemaSQL();
 
     if (updateDump) {
-      await this.orm.getSchemaGenerator().updateSchema();
+      await this._orm.getSchemaGenerator().updateSchema();
     } else {
       console.log('El esquema está actualizado.');
     }
@@ -23,7 +39,33 @@ export class AppService implements OnModuleInit, OnModuleDestroy {
 
   public async onModuleDestroy() {
     this.loggerService.log('Closing database connection');
-    await this.orm.close();
+    await this._orm.close();
     this.loggerService.log('Closed database connection');
+  }
+
+  public async seedUsers() {
+    // Seed adminUser
+    const users: CreateUserDto[] = [
+      {
+        provider: OAuthProvidersEnum.LOCAL,
+        email: 'david.misa97@gmail.com',
+        name: 'David Misael Villegas Sandoval',
+        password: this._configService.get('prefilledUserPassword', {infer: true}),
+        confirmed: true
+      }
+    ];
+
+    const promises = users.map(async (user) => {
+      try {
+
+        // Check if user already exists
+        const existingUser = await this._userService.findOneByEmail(user.email);
+        // If exist user, do nothing
+        if (existingUser) return;
+        await this._userService.create(user.provider, user.email, user.name, user.password, user.confirmed);
+      } catch (e) {
+        console.error(e);
+      }
+    });
   }
 }
